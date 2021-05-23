81c8d4f5126ccf6c05b5102f87fc97a1bf439a21
Aryaman Srivastav <22541378+blackk100@users.noreply.github.com>
2021-05-24 02:22:58 +0530

    Updated reverse tethering and screecasting procedure
    The batch files now assume the executables are present in the PATH environment variable.
    Links to get the executables are included, with additional information for gnirehtet setup.
    gnirehtet also uses Google DNS explicitly and socket 31415 to not conflict with BOINC (issue 338 on the gnirehtet repository).

--- Android/screencast.cmd
+++ Android/screencast.cmd
@@ -1,5 +1,6 @@
 @echo off
-F:
-cd F:\Code\Android\scrcpy\1.14\
-start scrcpy.exe
-exit
+REM Assumes adb and scrcpy are in the PATH environment variable. Get adb from: https://developer.android.com/studio/releases/platform-tools ; Get scrcpy from: https://github.com/Genymobile/scrcpy
+scrcpy
+echo.
+echo Press any key to close...
+pause > nul
