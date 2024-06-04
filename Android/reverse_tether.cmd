78c0fd7e6fa553229bfa9ef98943799cd75e5d68
Aryaman Srivastav <22541378+blackk100@users.noreply.github.com>
2024-06-04 01:28:43 -0500

    Update reverse_tether.cmd
    Clarify comments. Close connections when quitting.

--- Android/reverse_tether.cmd
+++ Android/reverse_tether.cmd
@@ -1,12 +1,13 @@
 @echo off
 
-REM Assumes adb and gnirehtet are in the PATH environment variable. Get adb from: https://developer.android.com/studio/releases/platform-tools ; Get gnirehtet from: https://github.com/Genymobile/gnirehtet
-REM Also assumes the GNIREHTET_APK environment variable is set to the gnirehtet.apk file in the gnirehtet directory.
+REM Assumes adb is in the PWD, the PATH variable (platform-tools folder) or the ADB variable (adb.exe executable). Get adb from: https://developer.android.com/studio/releases/platform-tools
+REM Assumes gnirehtet is in the PWD or the PATH variable. Get gnirehtet from: https://github.com/Genymobile/gnirehtet
+REM Assumes the GNIREHTET_APK environment variable is set to the gnirehtet.apk file in the gnirehtet directory.
 REM If an error message stating that port 31415 is already in use, change the port number below (31416 is the default, and is documented to conflict with certain applications: https://github.com/Genymobile/gnirehtet/issues/338 ).
-REM If Google DNS is unavailable, change the IP address below to the desired DNS server (Google DNS is the default).
 
-gnirehtet autorun -d 8.8.8.8 -p 31415
+gnirehtet autorun -p 31415
 
 echo.
 echo Press any key to close.
+gnirehtet stop
 pause > nul
